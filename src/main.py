b8638ef2393053864d4917b7e86208ff168ff8d7
lnicolli <lnicolli@c1r14s6.42lausanne.ch>
2024-02-04 13:57:26 +0100

    change data size

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import random
 from test import *
 
 def main():
-	data_size = 1000
+	data_size = 50000
 	data = List([random.randint(-1000, 1000) for _ in range(data_size)])
 	data_array = (ctypes.c_int * len(data))(*data)
 	exit(0)
