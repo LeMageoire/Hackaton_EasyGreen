a51c0149e6e6d548f3ead70bddedb95d9d8c22f9
MathisGuyot <91309271+MathisGuyot@users.noreply.github.com>
2024-02-04 08:15:50 +0100

    Creation of src modifications

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hackaton-42_020224
 Hackaton_42 sur la thématique du Green IT du point de vue du Software Developper
+
+## List of ideas :
+- [ ]
+
+## Configuration
+- [ ] the CI pipeline needs rights to do it
+
---
+++ feature1/autocomp.py
@@ -0,0 +1,56 @@
+import ast
+import os
+from subprocess import run, PIPE
+import shutil
+
+class DecoratorAdder(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        # This example assumes all functions are candidates for optimization.
+        # You might want to add conditions to filter specific functions.
+        node.decorator_list.append(ast.Name(id='jit', ctx=ast.Load()))
+        return node
+
+# 
+def modify_source_code(file_path):
+    with open(file_path, 'r') as file:
+        source_code = file.read()
+
+    tree = ast.parse(source_code)
+    tree = DecoratorAdder().visit(tree)
+    modified_code = ast.unparse(tree)
+
+    with open(file_path, 'w') as file:
+        file.write(modified_code)
+
+ # Function to measure execution time
+def measure_execution_time(executable):
+    start = time.time()
+    run(["python", executable])
+    end = time.time()
+    return end - start
+
+def benchmark_code(executable):
+    return measure_execution_time(executable)
+
+if __name__ == '__main__':
+    # Step 2: Modify source code (add @jit decorator to all functions) as a copy
+    source_code_directory = 'src'
+    if not os.path.exists('energy'):
+        os.makedirs('energy')
+    for root, dirs, files in os.walk(source_code_directory):
+        for file in files:
+            if file.endswith('.py'):
+                shutil.copy(os.path.join(root, file), 'energy')
+                modify_source_code(os.path.join(root, file))
+
+  # Step 1 : Measure performance before optimization
+    pre_time = benchmark_code("src")
+    # Step 3: Measure performance after optimization
+    post_time = benchmark_code("energy")
+    # Step 4: Commit changes if performance improved
+    if post_time < pre_time:
+        print("Success: The optimized code is faster")
+        exit(0)
+    else:
+        print("Failure: The optimized code is not faster")
+        exit(1)    
