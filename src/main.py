39844b49dcc1d604d22b102b61cd1b513c9eb94f
lnicolli <lnicolli@c1r14s6.42lausanne.ch>
2024-02-04 15:10:34 +0100

    fixing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from numba import jit
+import os
 from numba.typed import List
-import ctypes
 import random
 from test import *
 
 def main():
-	data_size = 50000
-	data = List([random.randint(-1000, 1000) for _ in range(data_size)])
-	data_array = (ctypes.c_int * len(data))(*data)
-	exit(0)
+    data_size = 50000
+    data = List([random.randint(-1000, 1000) for _ in range(data_size)])
+    quicksort(data,0, len(data) - 1)
+    print(100*"\n")
+    print("Thank you for all the fishs")
+    exit(0)
 
 if __name__ == '__main__':
-	main()
+    main()
